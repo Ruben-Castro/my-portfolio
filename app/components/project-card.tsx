2146b55cca570c35e376a272cf4a290be326e201
Ruben Castro-Espinoza <rubencastroe@gmail.com>
2023-05-09 20:02:46 -0700

    fixed key error

--- app/components/project-card.tsx
+++ app/components/project-card.tsx
@@ -46,8 +46,8 @@ const ProjectCard = ({
         </button>
       </div>
       <div className="px-6 pt-4 pb-2">
-        {techStack.map((tech) => (
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+        {techStack.map((tech, index) => (
+          <span key={index} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
             {tech}
           </span>
         ))}
